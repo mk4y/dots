079469b56767023734fbe382f674f5a5774aa636
Martin Kluge <mk4y9o@gmail.com>
2014-07-23 22:16:57 +0200

    update

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -236,11 +236,12 @@ function put_dots() {
 function update_dots() {
 #  emulate -LR zsh
 
-  pushd $DOTSPATH > /dev/null
 
   if [ `git diff --quiet` ]; then
     msg_info "no updates required"
   else
+    pushd $DOTSPATH > /dev/null
+
     git commit -am "update" > /dev/null
 
     git push origin master > /dev/null
